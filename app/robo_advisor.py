81b06f1f14b7f86d6443f2f285ff1e409c694bd2
Kevin Pinkerton <kevinhpinkerton@gmail.com>
2021-03-07 10:16:48 -0500

    Added 52-week high and lows.

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -14,7 +14,7 @@ def to_usd(price):
 print("Each ticker is inputted individually. Hit the return key once to continue, twice to finish.")
 
 api_key = os.getenv("ALPHAVANTAGE_API_KEY")
-columns = {'timestamp': str, 'open': float, 'high': float, 'low': float, 'close': float, 'volume': int}
+columns = {'timestamp': 'datetime64[ns]', 'open': float, 'high': float, 'low': float, 'close': float, 'volume': int}
 risk_factor = .2
 data = dict()
 
@@ -25,7 +25,7 @@ while True:
     elif len(ticker) < 1 or len(ticker) > 5 or not ticker.isalpha():
         print("Expecting a properly-formed stock symbol like 'MSFT'. Try again.")
     else:
-        request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}&apikey={api_key}"
+        request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}&outputsize=full&apikey={api_key}"
         try:
             response = requests.get(request_url)
             parsed_response = json.loads(response.text)
@@ -35,19 +35,26 @@ while True:
             df.columns = columns.keys()
             df = df.astype(columns)
 
+            # print(df)
             # df.to_csv(os.path.join(os.path.dirname(__file__), "..", "data", f"prices_{ticker.lower()}.csv"), index = False)
             plt.plot(df["timestamp"], df["close"], label = ticker)
 
-            if min(df['low']) * (1 + risk_factor) > df['close'][0]:
+            latest_day = df['timestamp'][0]
+            latest_close = df['close'][0]
+            last_year = dt.datetime(latest_day.year-1, latest_day.month, latest_day.day)
+            year_high = max(df.query("@last_year < `timestamp` <= @latest_day")["high"])
+            year_low = min(df.query("@last_year < `timestamp` <= @latest_day")["low"])
+
+            if year_low * (1 + risk_factor) > latest_close:
                 recommendation = "BUY"
             else:
                 recommendation = "DO NOT BUY"
                 
             data[ticker] = {
-                "LATEST DAY": df['timestamp'][0],
-                "LATEST CLOSE": to_usd(df['close'][0]),
-                "RECENT HIGH": to_usd(max(df['high'][:100])),
-                "RECENT LOW": to_usd(min(df['low'])),
+                "LATEST DAY": latest_day,
+                "LATEST CLOSE": to_usd(latest_close),
+                "52-WEEK HIGH": to_usd(year_high),
+                "52-WEEK LOW": to_usd(year_low),
                 "RECOMMENDATION": recommendation
             }
         except:
