c7adece467f3e9dae2a49b794591a0b8d4fc82a7
Kevin Pinkerton <kevinhpinkerton@gmail.com>
2021-03-07 11:32:34 -0500

    Optimized code.

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -35,42 +35,45 @@ while True:
         break
     elif len(ticker) < 1 or len(ticker) > 5 or not ticker.isalpha():
         print("Expecting a properly-formed stock symbol like 'MSFT'. Try again.")
+    elif ticker in data.keys():
+        pass
     else:
         request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}&outputsize=full&apikey={api_key}"
         try:
             response = requests.get(request_url)
             parsed_response = json.loads(response.text)
-
             df = pd.DataFrame.from_dict(parsed_response["Time Series (Daily)"], 'index')
-            df.reset_index(inplace = True)
-            df.columns = columns.keys()
-            df = df.astype(columns)
-
-            # print(df)
-            # df.to_csv(os.path.join(os.path.dirname(__file__), "..", "data", f"prices_{ticker.lower()}.csv"), index = False)
-            plt.plot(df["timestamp"], df["close"], label = ticker)
-
-            latest_day = df['timestamp'][0]
-            latest_close = df['close'][0]
-            last_year = dt.datetime(latest_day.year-1, latest_day.month, latest_day.day)
-            year_high = max(df.query("@last_year < `timestamp` <= @latest_day")["high"])
-            year_low = min(df.query("@last_year < `timestamp` <= @latest_day")["low"])
-
-            if year_low * (1 + risk_tolerance) > latest_close:
-                recommendation = "BUY"
-            else:
-                recommendation = "DO NOT BUY"
-                
-            data[ticker] = {
-                "LATEST DAY": latest_day,
-                "LATEST CLOSE": to_usd(latest_close),
-                "52-WEEK HIGH": to_usd(year_high),
-                "52-WEEK LOW": to_usd(year_low),
-                "RECOMMENDATION": recommendation
-            }
         except:
             print("Sorry, couldn't get any trading data for " + ticker + ".")
+            continue
 
+        df.reset_index(inplace = True)
+        df.columns = columns.keys()
+        df = df.astype(columns)
+
+        # print(df)
+        df.to_csv(os.path.join(os.path.dirname(__file__), "..", "data", f"prices_{ticker.lower()}.csv"), index = False)
+        plt.plot(df["timestamp"], df["close"], label = ticker)
+
+        latest_day = df['timestamp'][0]
+        latest_close = df['close'][0]
+        last_year = dt.datetime(latest_day.year-1, latest_day.month, latest_day.day)
+        year_high = max(df.query("@last_year < `timestamp` <= @latest_day")["high"])
+        year_low = min(df.query("@last_year < `timestamp` <= @latest_day")["low"])
+
+        if year_low * (1 + risk_tolerance) > latest_close:
+            recommendation = "BUY"
+        else:
+            recommendation = "DO NOT BUY"
+            
+        data[ticker] = {
+            "LATEST DAY": latest_day,
+            "LATEST CLOSE": to_usd(latest_close),
+            "52-WEEK HIGH": to_usd(year_high),
+            "52-WEEK LOW": to_usd(year_low),
+            "RECOMMENDATION": recommendation
+        }
+        
 print("-------------------------")
 print("SELECTED SYMBOLS: " + ", ".join(data.keys()))
 print("-------------------------")
